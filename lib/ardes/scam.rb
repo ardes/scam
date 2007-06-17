9e68526670f0ecc77e7e05bad0aa6aa4afb31cbc
ian <ian@845bbffb-5c18-0410-91b3-f25c072b94c1>
2007-06-17 12:24:39 +0000

    scam:latest changes
    git-svn-id: http://svn.ardes.com/rails_plugins/scam@344 845bbffb-5c18-0410-91b3-f25c072b94c1

--- lib/ardes/scam.rb
+++ lib/ardes/scam.rb
@@ -1,10 +1,52 @@
 class Scam < ActiveRecord::Base
+  class_inheritable_accessor :default_content_type
+  
   belongs_to :scammable, :polymorphic => true
   
   serialize :parsed_content
   
   validates_presence_of :name
   
+  # initialize parsed_content with {} on first read
+  def parsed_content
+    read_attribute(:parsed_content) || self.parsed_content = {}
+  end
+  
+  # reset parsed_content when content written
+  def content=(content)
+    self.parsed_content = {}
+    write_attribute(:content, content)
+  end
+  
+  # name is a symbol
+  def name
+    read_attribute(:name).to_sym rescue nil
+  end
+      
+  def method_missing(method, *args, &block)
+    if method.to_s =~ /^to_(.+)$/
+      content_type = $1.to_sym
+      returning(to_content(content_type)) do |parsed|
+        raise RuntimeError, "Unknown content type #{content_type}" unless parsed
+      end
+    else
+      super
+    end
+  end
+
+  def respond_to?(method)
+    super || method.to_s =~ /^to_.+$/
+  end
+  
+  def to_content(content_type = self.default_content_type)
+    content_type = content_type.to_sym if content_type.is_a?(String)
+    return parsed_content[content_type] if parsed_content[content_type]
+    parsed = content_type ? (send("parse_to_#{content_type}") rescue parse_to(content_type)) : parse_to(content_type)
+    returning parsed do |parsed|
+      update_attributes :parsed_content => self.parsed_content.merge(content_type => parsed)
+    end
+  end
+
   # Rake migration task to create the scam table
   def self.create_table(create_table_options = {})
     connection.create_table(table_name, create_table_options) do |t|
@@ -25,56 +67,8 @@ class Scam < ActiveRecord::Base
     connection.drop_table table_name
   end
   
-  # transform the passed object into a format suitable for content
-  def self.to_content(content)
-    content
-  end
-  
-  def to_content(content)
-    self.class.to_content(content)
-  end
-  
-  def parsed_content
-    read_attribute(:parsed_content) || self.parsed_content = {}
-  end
-  
-  def content=(content)
-    self.parsed_content = {}
-    write_attribute(:content, to_content(content))
-  end
-  
-  def name
-    read_attribute(:name).to_sym rescue nil
-  end
-  
-  def to_s
-    self.content
-  end
-    
-  def method_missing(method, *args, &block)
-    if method.to_s =~ /^to_(.+)$/
-      returning(to_parsed_content($1.to_sym)) {|p| raise NoMethodError unless p}
-    else
-      super
-    end
-  end
-
-  def respond_to?(method)
-    super || method.to_s =~ /^to_.+$/
-  end
-  
-  def to_parsed_content(content_type)
-    content_type = content_type.to_sym
-    return parsed_content[content_type] if parsed_content[content_type]
-      
-    parsed = (send("parse_#{content_type}") rescue parse(content_type))
-    returning parsed do |str|
-      update_attributes :parsed_content => self.parsed_content.merge(content_type => str)
-    end
-  end
-  
 protected
-  def parse(content_type, content = self.content)
+  def parse_to(content_type, content = self.content)
     content.to_s
   end
 end
--- spec/models/scam_spec.rb
+++ spec/models/scam_spec.rb
@@ -19,53 +19,56 @@ describe Scam, :shared => true do
     @scam.should_receive(:parsed_content=).with({})
     @scam.content = 'Gday'
   end
+  
+  it 'should call #to_content(:whatever) when sent #to_<whatever>' do
+    @scam.should_receive(:to_content).with(:whatever).and_return('parsed')
+    @scam.to_whatever
+  end
+  
+  it 'should raise RuntimeError if parse_to(:whatever) when sent #to_<whatever>' do
+    @scam.stub!(:parse_to).and_return(nil)
+    lambda { @scam.to_whatever }.should raise_error(RuntimeError)
+  end
+  
+  it { @scam.should respond_to(:to_whatever) }
 end
 
-describe Scam, '#to_<whatever> (without parsed_content[:whatever])' do
+describe Scam, '#to_content(:whatever) (without parsed_content[:whatever])' do
   before do
     @scam = Scam.new
-    @scam.stub!(:parse).and_return('parsed')
   end
   
-  it 'should respond to #to_whatever' do
-    @scam.should respond_to(:to_whatever)
+  it 'should call #parse_to_whatever' do
+    @scam.should_receive(:parse_to_whatever).once.and_return('parsed')
+    @scam.to_content(:whatever)
   end
   
-  it 'should call #parse_whatever' do
-    @scam.should_receive(:parse_whatever).once.and_return('parsed')
-    @scam.to_whatever
+  it 'should call parse_to(:whatever) if #parse_whatever is undefined' do
+    @scam.should_receive(:parse_to).with(:whatever).once.and_return('parsed')
+    @scam.to_content(:whatever)
   end
-  
-  it 'should call parse(:whatever) if #parse_whatever is not a method' do
-    @scam.should_receive(:parse).with(:whatever).once.and_return('parsed')
-    @scam.to_whatever
-  end
-  
-  it 'should store the results of parse(:whetever) in parsed_content[:whatever] and save record' do
+
+  it 'should store the results of parse_to(:whetever) in parsed_content[:whatever] and save record' do
+    @scam.stub!(:parse_to).and_return('parsed')
     @scam.should_receive(:save).once
-    @scam.to_whatever
+    @scam.to_content(:whatever)
     @scam.parsed_content[:whatever].should == 'parsed'
-  end
-  
-  it 'should raise NoMethodError if parse(:whatever) returns nil' do
-    @scam.stub!(:parse).and_return(nil)
-    lambda { @scam.to_whatever }.should raise_error(NoMethodError)
   end
 end
 
-describe Scam, '#to_<whatever> (with parsed_content[:whatever])' do
+describe Scam, '#to_content(:whatever) (with parsed_content[:whatever])' do
   before do
     @scam = Scam.new
     @scam.parsed_content[:whatever] = 'parsed'
   end
 
   it 'should return parsed_content[:whatever]' do
-    @scam.to_whatever.should == 'parsed'
+    @scam.to_content(:whatever).should == 'parsed'
   end
   
   it 'should not save record' do
     @scam.should_not_receive(:save)
-    @scam.to_whatever
+    @scam.to_content(:whatever)
   end
 end
 
@@ -77,8 +80,4 @@ describe Scam, '.new' do
   it 'should return {} for #parsed_content' do
     @scam.parsed_content.should == {}
   end
-  
-  it 'should have default_content_type'
-  
-  it 'should call parse(default_content_type) whne #to_s called'
 end
