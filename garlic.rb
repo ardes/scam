73ddf309deeace3d477979d3023a5ef0330720e1
Ian White <ian.w.white@gmail.com>
2009-02-28 15:39:46 +0000

    Added garlic build

---
+++ garlic.rb
@@ -0,0 +1,32 @@
+# typical rspec garlic configuration
+
+garlic do
+  # this plugin
+  repo "scam", :path => '.'
+  
+  # other repos
+  repo "rails", :url => "git://github.com/rails/rails"
+  repo "rspec", :url => "git://github.com/dchelimsky/rspec"
+  repo "rspec-rails", :url => "git://github.com/dchelimsky/rspec-rails"
+  
+  # target railses
+  ['origin/master', 'origin/2-2-stable', 'origin/2-1-stable', 'origin/2-0-stable'].each do |rails|
+    
+    # declare how to prepare, and run each CI target
+    target "Rails: #{rails}", :tree_ish => rails do
+      prepare do
+        plugin "scam", :clone => true # so we can work in targets
+        plugin "rspec"
+        plugin "rspec-rails" do
+          `script/generate rspec -f`
+        end
+      end
+    
+      run do
+        cd "vendor/plugins/scam" do
+          sh "rake"
+        end
+      end
+    end
+  end
+end
