e5a91a9ef442f38accdeeec2111ae01367a8bd1b
Ian White <ian.w.white@gmail.com>
2009-03-01 11:45:09 +0000

    Fix bug in scammable spec (wrong ivar name)

--- spec/models/scammable_shared.rb
+++ spec/models/scammable_shared.rb
@@ -1,10 +1,9 @@
-require File.expand_path(File.join(File.dirname(__FILE__), '../spec_helper'))
-require File.expand_path(File.join(File.dirname(__FILE__), '../app'))
-
 # set @scammable before using these shared examples
 describe 'Scammable with scams not loaded', :shared => true do
   it 'should not attempt to save scams on save' do
-    @scammable.scam_names.each {|scam_name| @scam_name.should_not_receive(scam_name) }
+    @scammable.scam_names.each do |scam_name|
+      @scammable.should_not_receive(scam_name)
+    end
     @scammable.save
   end
 end
