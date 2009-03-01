1f492936feacf9d99eba1579f2b602668ab00eb4
Ian White <ian.w.white@gmail.com>
2009-03-01 11:43:31 +0000

    Better name of scammable spec

--- spec/lib/scammable_spec.rb
+++ spec/lib/scammable_spec.rb
@@ -1,4 +1,5 @@
-require File.expand_path(File.join(File.dirname(__FILE__), '../spec_helper'))
+__DIR__ = File.expand_path(File.dirname(__FILE__))
+require File.expand_path("#{__DIR__}/../spec_helper")
 
 describe Ardes::Scammable, ' module' do
   it 'should have scam_class_name of "Scam"' do
